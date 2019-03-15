366807a555399a2a0ee2d18d2b57cb096f733dfc
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-15 21:41:59 +0000

    Updated source url

--- self-updating.sh
+++ self-updating.sh
@@ -8,7 +8,7 @@ SCRIPTNAME=`basename "$0"`
 # Storing the full path and filename in a variable:
 FULLSCRIPTPATH="`pwd`/$SCRIPTNAME"
 
-UPDATE_SOURCE="https://raw.githubusercontent.com/ashleycawley/my-bash-scripting-examples/master/self-updating.sh"
+UPDATE_SOURCE="https://raw.githubusercontent.com/ashleycawley/bash-self-updater/master/self-updating.sh"
 
 # URL to check to see if update should proceed
 TWOFA="http://status.ashleycawley.co.uk/update-2fa.txt"
